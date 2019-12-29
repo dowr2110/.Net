79200644437b4250bfcc69c9dc4040bcd8e68645
dowr2110 <dowr.atayew@mail.ru>
2019-12-30 00:11:06 +0300

    commit 16,2

--- laba 16/Program.cs
+++ laba 16/Program.cs
@@ -12,8 +12,8 @@ namespace laba_16
 
     class Program
     {
-        public static Task task;
-        public static bool second_task = false;
+        
+
         public static CancellationTokenSource cancelTokenSource;
         public static CancellationToken token;
         static BlockingCollection<string> BlockColl;
@@ -21,41 +21,43 @@ namespace laba_16
 
         static void Main(string[] args)
         {
-            Console.WriteLine("<--------------------> Задание 1 <-------------------->\n");
-            int row = 1000, col = 1000;//столбец и строка
-            int[,] matrix1 = new int[row, col];//создаем двухмерн массив (матрицу)
-            int[,] matrix2 = new int[row, col];//создаем двухмерн массив (матрицу)
-            int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];//создаем двухмерн массив (матрицу)//?
-
-            Console.WriteLine("Замеры генерации двух массивов 1000х1000");//?
-            Stopwatch stopWatch = new Stopwatch();//StopWatch???
-            for (int i = 0; i < 10; i++)
-            {
-                stopWatch.Restart();//??
-                CreateMatrix(ref matrix1, ref matrix2, row, col);// создаем матрицу
-                stopWatch.Stop();//??
-                Console.WriteLine($"Генерация {i + 1}\tВремя - {stopWatch.ElapsedMilliseconds}мс");//??
-            }
+            Stopwatch stopWatch = new Stopwatch();
+            //Console.WriteLine("\nЗадание 1\n");
 
 
-            task = new Task(() => Multiplication(matrix1, matrix2, ref matrix3));//умнажаем матрицу
-            task.Start();
+
+            Task task00 = new Task(()=>factorial(10));
+            task00.Start();
+            stopWatch.Start();
+
+            Console.WriteLine($"Статус задачи - {task00.Status}; ID - {task00.Id}");
+            task00.Wait();
+            stopWatch.Stop();
+            Console.WriteLine($"Статус задачи - {task00.Status}; Статус завершения - {task00.IsCompleted}");
+            
+            Console.WriteLine("Время : " + stopWatch.ElapsedMilliseconds+"мс");
+            task00.Dispose();
 
 
-            Console.WriteLine("<--------------------> Задание 2 <-------------------->\n");
+            Console.WriteLine("\nЗадание 2\n");
             cancelTokenSource = new CancellationTokenSource();
             token = cancelTokenSource.Token;
-            second_task = true;
-            task = new Task(() => Multiplication(matrix1, matrix2, ref matrix3));
-            task.Start();
-            task.Wait();
-
-            if (token.IsCancellationRequested)
+        
+            Task task01 = new Task(() => factorial(10));
+            task01.Start();
+            Console.WriteLine("Введите 'y' для отмены операции :");
+            string g = Console.ReadLine();
+            if (g=="y")
             {
-                Console.WriteLine("Task отменен");
+                cancelTokenSource.Cancel();
             }
-            task.Dispose();
-            Console.WriteLine("<--------------------> Задание 3 <-------------------->\n");
+            task01.Wait();
+            task01.Dispose();
+            
+           
+
+
+            Console.WriteLine("\nЗадание 3\n");
 
             var task1 = Task.Factory.StartNew<int>(() => task11(10));//создаем использовании статического метода Task.Factory.StartNew()
             var task2 = Task.Factory.StartNew<int>(() => task22(10));
@@ -65,15 +67,16 @@ namespace laba_16
             task4.Wait();//Wait Ожидает завершения выполнения задачи Task(конкретной).
             task1.Dispose();//освобождает
             task2.Dispose();
+
             task3.Dispose();
 
-            Console.WriteLine("<--------------------> Задание 4 <-------------------->\n");
+            Console.WriteLine("\nЗадание 4 \n");
             //задача продолжения 
             Task task5 = new Task(() => { Console.WriteLine($"ID: {Task.CurrentId}"); });
             Task task6 = task5.ContinueWith(task55);//ContinueWith, который в качестве параметра принимает делегат Action<Task>. То есть метод, который передается в данный метод в качестве значения параметра, должен принимать параметр типа Task.
             task5.Start();
-            task6.Wait();
-            task5.Dispose();
+            task6.Wait();//ожидавет завершение выполнение задачи таск
+            task5.Dispose();//освобождает
             task6.Dispose();
 
             var task7 = Task.Factory.StartNew<int>(() => task11(10));////создаем использовании статического метода Task.Factory.StartNew()
@@ -87,7 +90,7 @@ namespace laba_16
                 task8.Dispose();
             }
             task7.Dispose();
-            Console.WriteLine("<--------------------> Задание 5 <-------------------->\n");
+            Console.WriteLine("\nЗадание 5\n");
             int[] arr1 = new int[1000000];//генерация нескольких массивов по 1000000 элементов,
             int[] arr2 = new int[1000000];
             Random random = new Random();
@@ -99,30 +102,31 @@ namespace laba_16
                 arr2[i] = random.Next(0, 200);
             }
             stopWatch.Stop();
-            Console.WriteLine($"Генерация 2х массивов в for \tВремя - {stopWatch.ElapsedMilliseconds}мс");
+            Console.WriteLine($"Генерация 2х массивов в for \tВремя - {stopWatch.ElapsedMilliseconds}мс");//смотрим с обычный For
             int[] arr3 = new int[1000000];
             int[] arr4 = new int[1000000];
             stopWatch.Restart();
-            Parallel.For(0, arr3.Length, i => {
+            Parallel.For(0, arr3.Length, i =>
+            {
                 arr3[i] = random.Next(0, 200);
                 arr4[i] = random.Next(0, 200);
             });
             stopWatch.Stop();
-            Console.WriteLine($"Генерация 2х массивов в ParallelFor \tВремя - {stopWatch.ElapsedMilliseconds}мс\n");
+            Console.WriteLine($"Генерация 2х массивов в ParallelFor \tВремя - {stopWatch.ElapsedMilliseconds}мс\n");//смотрим с parallelFor
             /*Метод Parallel.For позволяет выполнять итерации цикла параллельно. Он имеет следующее определение: For(int, int, Action<int>), 
              * где первый параметр задает начальный индекс элемента в цикле, а второй параметр - конечный индекс.
              * Третий параметр - делегат Action - указывает на метод, который будет выполняться один раз за итерацию:*/
-            Console.WriteLine("<--------------------> Задание 6 <-------------------->\n");
+            Console.WriteLine("Задание 6\n");
             Parallel.Invoke(Inv1, Inv2);
             /*Метод Parallel.Invoke в качестве параметра принимает массив объектов Action, 
              * то есть мы можем передать в данный метод набор методов, 
              * которые будут вызываться при его выполнении.
-             * Количество методов может быть различным, но в данном случае мы определяем выполнение трех методов.
+             * Количество методов может быть различным, 
              * Опять же как и в случае с классом Task мы можем передать либо название метода, либо лямбда-выражение.
 
 И таким образом, при наличии нескольких ядер на целевой машине данные
 методы будут выполняться параллельно на различных ядрах.*/
-            Console.WriteLine("<--------------------> Задание 7 <-------------------->\n");
+            Console.WriteLine("\nЗадание 7>\n");
             BlockColl = new BlockingCollection<string>(5);
             //это потокобезопасный класс коллекции
 
@@ -135,10 +139,10 @@ namespace laba_16
             Sup.Dispose();
             Con.Dispose();
 
-            Console.WriteLine("<--------------------> Задание 8 <-------------------->\n");
+            Console.WriteLine("\nЗадание 8 \n");
 
             AsyncEvenNum();
-      
+
         }
 
         static async void AsyncEvenNum()
@@ -164,7 +168,7 @@ namespace laba_16
             Random random = new Random();
 
             int x, z;
-            List<string> products = new List<string>() { "Холодильник", "Плита", "Стиральная машина", "Пылесос", "Утюг" };
+            List<string> products = new List<string>() { "стул", "стол", "кровать", "шкаф", "тумба" };
             for (int i = 0; i < 5; i++)
             {
                 x = random.Next(0, products.Count - 1);
@@ -245,46 +249,39 @@ namespace laba_16
             Console.WriteLine($"Результат: {res}");
         }
 
-        public static void CreateMatrix(ref int[,] matrix1, ref int[,] matrix2, int row, int col)//создаем матрицу
+
+        static void factorial(int n)
         {
-            Random random = new Random();
 
-            for (int i = 0; i < row; i++)
+            int result = 1;
+            for (int i = 1; i <= n; i++)
             {
-                for (int j = 0; j < col; j++)
-                {
-                    matrix1[i, j] = random.Next(0, 100);
-                    matrix2[i, j] = random.Next(0, 100);
-                }
-            }
 
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+
+                result *= i;
+                Console.WriteLine($"{i}! = "+result);
+                Thread.Sleep(500);
+            }
+           
         }
 
-        private static void Multiplication(int[,] matrix1, int[,] matrix2, ref int[,] matrix3)//умнажаем матрицы
-        {
-            Console.WriteLine($"\nID задачи - {task.Id}");
-            for (int i = 0; i < matrix1.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix2.GetLength(1); j++)
-                {
-                    for (int k = 0; k < matrix2.GetLength(0); k++)
-                    {
-                        matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
-                    }
-                }
-                if (i == matrix1.GetLength(0) / 2)
-                {
-                    Console.WriteLine($"Статус задачи - {task.Status}; Статус завершения - {task.IsCompleted}");
-                }
-                if (i == matrix1.GetLength(0) / 5 && second_task)
-                {
-                    cancelTokenSource.Cancel();
-                    if (token.IsCancellationRequested)
-                    {
-                        return;
-                    }
-                }
-            }
-        }
+
+
+
     }
 }
+
+
+/*1) Создание объекта Task и вызова у него метода Start
+Task task = new Task(() => Console.WriteLine("Hello Task!"));
+task.Start();
+2) использовании статического метода Task.Factory.StartNew()
+Task task = Task.Factory.StartNew(() => Console.WriteLine("Hello Task!"));
+3) использование статического метода Task.Run()
+Task task = Task.Run(() => Console.WriteLine("Hello Task!"));
+*/
